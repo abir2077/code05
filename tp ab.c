701968ef9c63ce27e4ab94fa3811ff7de3e8ee9e
abir2077 <bekkarabir20@gmail.com>
2024-10-23 20:08:45 +0100

    Add files via upload

---
+++ tp ab.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main () { int i, j ;
+char M[5][5]  = {
+{'1','2','3','4','5'},{'7','a','c','8','d'} ,{'c','9','4','z','8'} , {'5','6','p','n','3'} , {'2','9','t','m','k'} } ;
+for ( i=0; i<5 ; i++) {
+for (j=0; j<5;j++){
+printf("%c",M[i][j] ) ;
+}
+printf("\n");
+}
+printf("donner des lignes d'indices pair :\n");
+for( i=0; i<5 ;i+=2) {
+for(j=0;j<5;j++){
+printf("%c",M[i][j]);
+}
+printf("\n");
+}
+printf("donner les éléments d' indices impair de chaque lignes :\n");
+for(i=0; i<5 ; i++) {
+for(j=0; j<5;j+=2){
+printf("%c",M[i][j]) ;
+}
+printf("\n");
+}
+return 0;
+}
