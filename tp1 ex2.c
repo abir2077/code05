fbd12b84e66521acafb69c638ba4ae129c6ca5d8
abir2077 <bekkarabir20@gmail.com>
2024-11-06 21:29:58 +0100

    Update tp1 ex2.c

--- tp1 ex2.c
+++ tp1 ex2.c
@@ -1,27 +1,40 @@
 #include<stdio.h>
-int main () { int i, j ;
-int Mat[4][4] = {
-{1,2,3,4}, {5,6,7,8},{9,10,11,12},{13,14,15,16} } ;
-printf("donner le matrice:\n") ;
-for(i=0;i<4;i++) {
-for(j=0;j<4;j++) {
-printf("%d\t",Mat[i][j]);
-}
-printf("\n") ;
-}
-for(i=0;i<4;i++){
-for(j=i+1;j<4;j++){
-int temp = Mat[i][j] ;
-Mat[i][j]  =  Mat[j][i] ;
-Mat[j][i] = temp ;
-}
-}
-printf("donner le matrice après échange de traingle:\n") ;
-for(i=0;i<4;i++){
-for(j=0;j<4;j++){
-printf("%d\t",Mat[i][j]) ;
-}
-printf("\n");
-}
+int main () 
+{ 
+    int i, j ;
+  
+    int Mat[4][4] =
+     {
+         {1,2,3,4}, {5,6,7,8},{9,10,11,12},{13,14,15,16} 
+     } ;
+  printf("donner le matrice:\n") ;
+      for(i=0;i<4;i++)
+        {
+            for(j=0;j<4;j++) 
+               {
+                  printf("%d\t",Mat[i][j]);
+               }
+           printf("\n") ;
+        }
+            for(i=0;i<4;i++)
+               {
+                   for(j=i+1;j<4;j++)
+                       {
+                          int temp = Mat[i][j] ;
+                         
+                          Mat[i][j]  =  Mat[j][i] ;
+                         
+                          Mat[j][i] = temp ;
+                       }
+                }
+  printf("donner le matrice aprÃ¨s Ã©change de traingle:\n") ;
+      for(i=0;i<4;i++) 
+          {
+            for(j=0;j<4;j++)
+                {
+                   printf("%d\t",Mat[i][j]) ;
+                }
+              printf("\n");
+         }
 return 0 ;
 }
